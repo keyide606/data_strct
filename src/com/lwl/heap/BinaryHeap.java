d274fe3cd6b3a6a8076619f1661ea019eb2a1c34
keyide <251438278@qq.com>
2024-03-17 14:23:08 +0800

    '实现clear,isEmpty方法'

--- src/com/lwl/heap/BinaryHeap.java
+++ src/com/lwl/heap/BinaryHeap.java
@@ -1,25 +1,47 @@
 package com.lwl.heap;
 
+import java.util.Comparator;
+
 /**
  * 二叉堆的实现
+ *
  * @param <E>
  */
-public class BinaryHeap<E> implements Heap<E>{
+public class BinaryHeap<E> implements Heap<E> {
 
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private E[] elements;
+
+    private int size;
+
+    private Comparator<E> comparator;
+
+    public BinaryHeap(Comparator<E> comparator) {
+        this.comparator = comparator;
+        elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public BinaryHeap() {
+        this(null);
+    }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
     }
 
     @Override
@@ -29,7 +51,8 @@ public class BinaryHeap<E> implements Heap<E>{
 
     @Override
     public E get() {
-        return null;
+        emptyCheck();
+        return elements[0];
     }
 
     @Override
@@ -40,5 +63,16 @@ public class BinaryHeap<E> implements Heap<E>{
     @Override
     public E replace(E element) {
         return null;
+    }
+
+
+    private int compare(E o1, E o2) {
+        return comparator != null ? comparator.compare(o1, o2) : ((Comparable) o1).compareTo(o2);
+    }
+
+    private void emptyCheck() {
+        if (size <= 0) {
+            throw new ArrayIndexOutOfBoundsException("heap is empty");
+        }
     }
 }
